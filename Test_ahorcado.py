e2af2cc0a0c5850bc564fc7b1b146de84cd25c50
Damian Maximiliano LACOMBA <dlacomba@labturner.com.ar>
2020-07-01 10:22:02 -0300

    actualizacion

--- Test_ahorcado.py
+++ Test_ahorcado.py
@@ -1,15 +1,66 @@
 import unittest
-
 from ahorcado import Juego
 
 
 class AhorcadoTest(unittest.TestCase):
     def setUp(self):
         self.juego = Juego()
+        
 
-    def test(self):
-        letra="a"
-        result= self.juego.validar_letra(letra)
-        self.assertEqual(True, result)
+    def test_palabra_gato_probarletra_A(self):
+        self.juego.set_palabra("gato")
+        letra = "a"
+        resultado = self.juego.validar_letra(letra)
+        self.assertEqual(True,resultado)
+    
+    def test_nombreBienvenida_nombre_minus(self):
+        bienvenida = self.juego.bienvenida("juan")
+        self.assertEqual("Hola Juan",bienvenida)
+
+    def test_ingreso_gato_juego_ganador_perfecto(self):
+        letras_seleccionadas=["a","g","t","o"]
+        self.juego.set_palabra("gato")
+        self.probar_letras(letras_seleccionadas)
+        resultado = self.juego.resultado
+        self.assertEqual("ganador",resultado) 
+    
+    def test_ingreso_gato_perdedor(self):
+        letras_seleccionadas=["p","r","u","e","s","i","l"]
+        self.juego.set_palabra("gato")
+        self.probar_letras(letras_seleccionadas)
+        resultado = self.juego.resultado
+        self.assertEqual("perdedor",resultado) 
+
+    def test_ingreso_gato_juego_ganador_parcial(self):
+        letras_seleccionadas=["a","r","g","t","e","o"]
+        self.juego.set_palabra("gato")
+        self.probar_letras(letras_seleccionadas)
+        resultado = self.juego.resultado
+        self.assertEqual("ganador",resultado)
+
+    def test_ingreso_gato_juego_perdedor_parcial(self):
+        letras_seleccionadas=["a","r","g"]
+        self.juego.set_palabra("gato")
+        self.probar_letras(letras_seleccionadas)
+        resultado = self.juego.resultado
+        self.assertEqual("",resultado) 
+
+    def probar_letras(self,letras_seleccionadas):
+        for letra in letras_seleccionadas:
+            self.juego.jugar(letra)
+    
+    def test_completar_palabra(self):
+        letras_seleccionadas = ["g","t"]
+        self.juego.set_palabra("gato")
+        self.probar_letras(letras_seleccionadas)
+        palabra_completada = self.juego.palabra_usuario
+        self.assertEqual (["g","_","t","_"],palabra_completada)
+
+    def test_completar_palabra_completa(self):
+        letras_seleccionadas = ["g","t","a"]
+        self.juego.set_palabra("gata")
+        self.probar_letras(letras_seleccionadas)
+        palabra_completada = self.juego.palabra_usuario
+        self.assertEqual (["g","a","t","a"],palabra_completada)
 
    
--- ahorcado.py
+++ ahorcado.py
@@ -2,17 +2,70 @@ class Juego(object):
     
     
     def __init__(self):
-        self.palabra = "gato"
+        self.palabras = []
+        self.palabra = ""
+        self.intentos = 6
+        self.letras = []
+        self.resultado = ""
+        self.palabra_usuario = []
     
+    def set_palabra(self,palabra):
+        self.palabra = palabra
+        self.set_palabra_usuario(palabra)
+        
+    def set_palabra_usuario(self,palabra):
+        self.palabra_usuario = [ "_" for i in range(len(palabra))]
+        
+    
+
+    def completar_palabra(self,letra):
+        
+        index = self.palabra.find(letra)
+        while index !=-1:
+            self.palabra_usuario[index] = letra
+            index = self.palabra.find(letra,index+1,len(self.palabra))
+            
+
+
+    
+
+
     def validar_letra(self,letra):
         if letra in self.palabra:
             return True
         else:
             return False
+        
+    def bienvenida(self,nombre):
+        nombre = nombre.capitalize()
+        return ("Hola {}".format(nombre))
+
     
-    
+    def gano(self):
+        palabra_usuario = "".join(self.palabra_usuario)
+        if self.palabra == palabra_usuario:
+            return True
+        else:
+            return False
+
+  
+    def jugar(self,letra):
+        self.letras.append(letra)
+        if self.validar_letra(letra):
+            self.completar_palabra(letra)
+            if self.gano():
+                self.resultado = "ganador"
+        else:
+            self.intentos -=1
+        
+        if self.intentos == 0:
+            self.resultado = "perdedor"
+        
+
+        
+
+               
+
+
                 
 
-
-
-   
