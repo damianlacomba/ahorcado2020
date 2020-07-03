a0d39fde60949620cc293e3356e2cc48cd1bfc5f
damian <damianlacomba20@gmail.com>
2020-07-02 21:08:56 -0300

    Ultimate

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template,flash, request,g,redirect,url_for
+from flask import Flask, render_template,flash, request,g,redirect,url_for,session
 from ahorcado import Juego
 
 
 app = Flask(__name__)
-app.debug = True
+app.secret_key = 'any random string'
 
+juego = Juego()
 
+@app.before_request
+def before_request_user():
+    if "user" in session:
+        print(session["user"])
+    else:
+        redirect(url_for("index"))
 
 
 
@@ -13,44 +20,45 @@ app.debug = True
 def index():
     if request.method == "POST" and request.form['user']!="":
         user = request.form['user']
-        print(user)
+        session["user"] = user
+        session["words"] = []
+        """print(user)
         global juego
         juego = Juego()
         juego.set_jugador(user)
         print(juego.jugador)
-        juego.set_palabra("HAMACA")
+        juego.set_palabra("HAMACA")"""
         return redirect(url_for("play"))
     return render_template('index.html')
 
-@app.route("/jugar")
+
+
+@app.route("/jugar",methods=["POST","GET"])
 def play():
-    
+    juego = Juego()
+    juego.set_jugador(session["user"])
+    juego.set_palabra("DINOSAURIO")
+
+    if "words" in session:
+        print(session["words"])
+        for word in session["words"]:
+            juego.jugar(word)
+    session["resultado"] = juego.resultado
     return render_template("juego.html", juego = juego)
 
 @app.route('/jugar/<letra>')
 def seleccionar_letra(letra):
-    print(juego.letras)
-    print(letra)
-    juego.jugar(letra)
-    print(juego.letras)
-    print(juego.palabra_usuario)
+    if session["resultado"]=="":
+        temp = session["words"]
+        temp.append(letra)
+        session["words"] = temp
+        print(session["words"])
     return redirect(url_for("play"))
-    
 
-"""@app.route('/')
-def home():
-    global juego
-    juego = Juego()
-    juego.set_palabra("AVION")
-    print(juego.letras)
-    return render_template('ahorcado.html', juego=juego)
+@app.route('/resetear')
+def reset():
+    session["words"]=[]
+    return redirect(url_for("play"))
 
-@app.route('/<letra>')
-def seleccionar_letra(letra):
-    print(juego.letras)
-    print(letra)
-    juego.jugar(letra)
-    return render_template('ahorcado.html', juego = juego)"""
+
   
-if __name__ == "__main__":
-    app.run(debug=True)
