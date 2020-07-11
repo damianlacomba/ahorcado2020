e8f79321c7a404242b4e1ec6868d4e455608e10c
damian <damianlacomba20@gmail.com>
2020-07-11 12:50:24 -0300

    prueba_v8

--- features/steps/ganador_perfecto.py
+++ features/steps/ganador_perfecto.py
@@ -29,3 +29,55 @@ def win(context):
     element = context.browser.find_element_by_class_name("winner")
 
     assert element != None
+
+
+
+@when('ingresa 6 veces letras incorrectas')
+def incorrect_words(context):
+    for i in ["H","I","J","K","L","Y"]:
+        element = context.browser.find_element_by_id(i)
+        element.click()
+        time.sleep(3)
+    element = context.browser.find_element_by_id("word")
+    palabra = element.text
+    
+    assert palabra == "G A T O"
+
+@then('el resultado es perdedor')
+def step_impl(context):
+    time.sleep(5)
+    element = context.browser.find_element_by_class_name("loser")
+
+    assert element != None
+
+@when('ingresa letras correctas e incorrectas')
+def step_impl(context):
+    for i in ["H","G","J","A","L","Y","T","O"]:
+        element = context.browser.find_element_by_id(i)
+        element.click()
+        time.sleep(3)
+    element = context.browser.find_element_by_id("word")
+    palabra = element.text
+    
+    assert palabra == "G A T O"
+
+@given('un ganador')
+def step_impl(context):
+    context.browser.get('https://agiles20-ahorcado.herokuapp.com/jugar/')
+    time.sleep(3)
+    element = context.browser.find_element_by_class_name("user")
+    element.send_keys("juani")
+    context.browser.find_element_by_class_name("play").click()
+    time.sleep(5)
+    jugador = context.browser.find_element_by_tag_name("h1").text
+
+    for i in ["G","A","T","O"]:
+        element = context.browser.find_element_by_id(i)
+        element.click()
+        time.sleep(2)
+    element = context.browser.find_element_by_id("word")
+    palabra = element.text
+    
+    assert palabra == "G A T O"
+
+    
