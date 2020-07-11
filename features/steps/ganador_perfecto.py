74996fc971e2648e336517cc521fce8b57d9f1f1
damian <damianlacomba20@gmail.com>
2020-07-11 12:46:11 -0300

    prueba_v7

--- features/steps/ganador_perfecto.py
+++ features/steps/ganador_perfecto.py
@@ -1,25 +1,24 @@
 from behave import given, when, then
-from features import environment
 import time
 
 @given('un jugador')
 def welcome(context):
     context.browser.get('https://agiles20-ahorcado.herokuapp.com/jugar/')
     time.sleep(3)
-    element = environment.driver.find_element_by_class_name("user")
+    element = context.browser.find_element_by_class_name("user")
     element.send_keys("juani")
-    environment.driver.find_element_by_class_name("play").click()
+    context.browser.find_element_by_class_name("play").click()
     time.sleep(5)
-    jugador = environment.driver.find_element_by_tag_name("h1").text
+    jugador = context.browser.find_element_by_tag_name("h1").text
 
 
 @when('ingresa correctamente todas las letras')
 def correct_words(context):
     for i in ["G","A","T","O"]:
-        element = environment.driver.find_element_by_id(i)
+        element = context.browser.find_element_by_id(i)
         element.enviroment.click()
         time.sleep(2)
-    element = environment.driver.find_element_by_id("word")
+    element = context.browser.find_element_by_id("word")
     palabra = element.text
     
     assert palabra == "G A T O"
@@ -27,6 +26,6 @@ def correct_words(context):
 @then('el resultado es ganador')
 def win(context):
     time.sleep(5)
-    element = environment.driver.find_element_by_class_name("winner")
+    element = context.browser.find_element_by_class_name("winner")
 
     assert element != None
